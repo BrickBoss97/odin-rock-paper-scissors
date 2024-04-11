8099d6e36a29ecd3a2fc70c57f3dea3eb5f370f0
BrickBoss97 <unknownyoungster@gmail.com>
2024-04-11 16:33:54 -0700

    Refactor code to work with UI elements

--- script.js
+++ script.js
@@ -1,37 +1,42 @@
 const choices = ["rock", "paper", "scissors"]
-const winners = [];
+let winners = [];
+const rockBtn = document.querySelector("#rock");
+const paperBtn = document.querySelector("#paper");
+const scissorsBtn = document.querySelector("#scissors");
+const resetBtn = document.querySelector("#resetBtn");
+const roundMessage = document.querySelector("#round");
+const gameMessage = document.querySelector("#game");
+const resetMessage = document.querySelector("#reset");
+const playerScore = document.querySelector("#player");
+const computerScore = document.querySelector("#computer");
 
-function getPlayerSelection() {
-    let input = prompt ("Choose 'Rock', 'Paper', or 'Scissors'.")
-    while (input == null) {
-        input = prompt ("Choose 'Rock', 'Paper', or 'Scissors'.")
-    }
-    input = input.toLowerCase()
-    let check = validateInput(input);
-    while (check == false) {
-        input = prompt ("Choose 'Rock', 'Paper', or 'Scissors'. Spelling needs to be exact, but caplitalization doesn't matter.");
-        while (input == null) {
-            input = prompt ("Choose 'Rock', 'Paper', or 'Scissors'.")
-        }
-        input = input.toLowerCase()
-        check = validateInput(input);
-    }
-    return input
-}
 
-function validateInput(choice) {
-    return choices.includes(choice)
+rockBtn.addEventListener("click", clickListener);
+paperBtn.addEventListener("click", clickListener);
+scissorsBtn.addEventListener("click", clickListener);
+resetBtn.addEventListener("click", reset);
+
+function clickListener(event) {
+    let input = event.target.id;
+    if (checkScores() === true) {
+        resetMessage.style.display = "flex";
+        return
+    }
+    playRound(input);
+    checkScores();
 }
 
 function getComputerChoice() {
     return choices[Math.floor(Math.random() * choices.length)]
 }
 
-function playRound() {
-    const playerSelection = getPlayerSelection()
+function playRound(input) {
+    const playerSelection = input
     const computerSelection = getComputerChoice()
     const winner = checkWinner(playerSelection, computerSelection)
     winners.push(winner);
+    roundMessage.textContent = winner
+    roundMessage.style.display = "flex";
     return winner
 }   
 
@@ -45,22 +50,33 @@ function checkWinner(choiceP, choiceC) {
     return `You Lose, ${choiceP} loses to ${choiceC}.`
 }
 
-function playGame() {
-    for (let round = 1; round <= 5; round++) {
-        const winner = playRound()
-        console.log(winner)
-    }
-    logWins()
-}
-
-function logWins() {
+function checkScores() {
     let playerWins = winners.filter((item) => item.includes("Win") == true).length;
     let computerWins = winners.filter((item) => item.includes("Lose") == true).length;
-    let ties = winners.filter((item) => item.includes("Tie") == true).length;
-    console.group("Results:")
-    console.log("Player Wins:", playerWins);
-    console.log("Computer Wins:", computerWins);
-    console.log("Ties:", ties);
+    playerScore.textContent = `Player Score: ${playerWins}`;
+    computerScore.textContent = `Computer Score: ${computerWins}`;
+    if (playerWins >= 5) {
+        gameMessage.textContent = "You Win! You reached 5 points!"
+        gameMessage.style.backgroundColor = "rgb(99, 251, 97)";
+        gameMessage.style.display = "flex";
+        return true
+    }
+    else if (computerWins >= 5) {
+        gameMessage.textContent = "You Lose. The computer reached 5 points."
+        gameMessage.style.backgroundColor = "rgb(248, 97, 97)";
+        gameMessage.style.display = "flex";
+        return true
+    }
+    return false
 }
 
-playGame()
+function reset() {
+    playerScore.textContent = `Player Score: 0`;
+    computerScore.textContent = `Computer Score: 0`;
+    roundMessage.style.display= "none";
+    roundMessage.textContent = "";
+    gameMessage.style.display= "none";
+    gameMessage.textContent = "";
+    resetMessage.style.display= "none";
+    winners = []
+}
